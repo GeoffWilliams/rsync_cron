b963f05bc36c1942bf6d37b6c3ca7999476d95fc
Geoff Williams <geoff@geoffwilliams.me.uk>
2015-10-25 18:52:13 +1100

    move to real dir

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,6 @@ class rsync_cron::params {
   $user     = "rsync"
   $group    = $user
   $mode     = "0700"
-  $dir      = "/srv/test"
+  $dir      = "/srv/files"
   $log_file = "/var/log/rsync_cron.log"
 }
